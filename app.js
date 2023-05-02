89eb6909fa9b36893d1725bbddce0576322998dd
WeiWayne1030 <wayne10032004@gmail.com>
2023-05-03 00:52:48 +0800

    feat: add update & delete  todo function

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser')
 
 const PORT = process.env.PORT || 3000
 const mongoose = require('mongoose')
+// 載入 method-override(method功能)
+const methodOverride = require('method-override') 
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
@@ -16,6 +18,8 @@ mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTop
 
 //設定static file
 app.use(express.static('public'))
+// 設定每一筆請求都會透過 methodOverride 進行前置處理
+app.use(methodOverride('_method'))
 
 // 取得資料庫連線狀態
 const db = mongoose.connection
@@ -50,6 +54,7 @@ app.get('/', (req, res) => {
   .catch(err => console.log(err))
 })
 
+//新增餐廳
 app.get ('/records/new', (req, res) => {
   return res.render('new')
 })
@@ -60,4 +65,50 @@ Record
   .create({record})
   .then(() => res.redirect('/'))
   .catch(err => {console.log(err)})
+})
+
+//修改支出
+app.get('/records/:id/edit', (req, res) => {
+  const _id = req.params.id
+  return Record.findOne({ _id })
+    .lean()
+    .then((record) => res.render('edit', { record }))
+    .catch(err => {
+      console.log(err)
+      res.render(
+      'errorPage', 
+      { error: err.message }
+      )
+    })
+})
+app.post('/records/:id/edit', (req, res) =>{
+  const _id = req.params.id
+  return Record.findOne({ _id })
+    .then(record => {
+      record = Object.assign(record, req.body)
+      return record.save()
+    })
+    .then(()=> res.redirect('/'))
+    .catch(err => {
+      console.log(err)
+      res.render(
+      'errorPage', 
+      { error: err.message }
+      )
+    })
+})
+
+//刪除特定餐廳
+app.post('/records/:id/delete', (req, res) => {
+  const _id = req.params.id
+  return Record.findOne({ _id })
+    .then(record => { record.remove() })
+    .then(() => res.redirect('/'))
+    .catch(err => {
+      console.log(err)
+      res.render(
+      'errorPage', 
+      { error: err.message }
+      )
+    })
 })
