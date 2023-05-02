e6e2a3eb99dc7d2d53a61091aba57498ac52e7f6
WeiWayne1030 <wayne10032004@gmail.com>
2023-05-02 14:26:30 +0800

    feat: add create record function

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 
 const PORT = process.env.PORT || 3000
 const mongoose = require('mongoose')
@@ -31,6 +32,9 @@ db.once('open', () => {
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+// 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
+app.use(bodyParser.urlencoded({ extended: true }))
+
 //設定路由
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
@@ -38,8 +42,22 @@ app.listen(PORT, () => {
 
 //測試首頁
 app.get('/', (req, res) => {
-  Record.find()
+  Record
+  .find()
   .lean()
+  .sort({ _id: "asc" })
   .then(records => res.render('index', {records}))
   .catch(err => console.log(err))
+})
+
+app.get ('/records/new', (req, res) => {
+  return res.render('new')
+})
+
+app.post('/records', (req, res) => {
+  const record= req.body
+Record
+  .create({record})
+  .then(() => res.redirect('/'))
+  .catch(err => {console.log(err)})
 })
